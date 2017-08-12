c4e9b8bb7122359c60143b35412e9811042e5550
Ken Wagner <ken_xwagner@hotmail.com>
2017-08-12 00:14:56 -0700

    Updated
    Added a fact to the list of strings.

--- app/src/main/java/built2gsd/com/edisonfacts/FactList.java
+++ app/src/main/java/built2gsd/com/edisonfacts/FactList.java
@@ -3,7 +3,7 @@ package built2gsd.com.edisonfacts;
 import java.util.Random;
 
 /**
- * Created by ken.wagner on 9/17/2015.
+ * Created by ken.wagner on 8/12/2017.
  */
 public class FactList {
 
@@ -16,7 +16,7 @@ public class FactList {
             "When he was 12 he wrote and published the Grand Trunk Herald in 1862 which was sent to 400 railroad employees. ",
             "Edison built his first science lab in the his basement when he was 10. ",
             "Edison age 15 saved Jimmie MacKenzie age 3 from a runaway boxcar. MacKenzie's dad taught Edison to operate a telegraph. ",
-            "Edison worked with Henry Ford to create an electric car. ",
+            "Edison worked with Henry Ford to create an electric car.  ",
             "His first two kids had the nicknames Dot and Dash.",
             "Edison presented a paper on his pyromagnetic motor and generator at the August 1887 meeting of the American Association for the Advancement of Science.",
             "In 1880 Edison patented a magnetic separator after experiments he conducted with chemists Alfred Haid and Otto Moses (among others).",
@@ -25,12 +25,14 @@ public class FactList {
             "Lewis Latimer worked for Edison as an engineer and as an expert in patent litigation.",
             "December 18 1913: Orville Wright visited Edison at his West Orange Labs",
             "Edison was 22 years old when he filed his first patent for the Electrographic Vote Recorder. It was never used.",
+			"Edison encouraged Henry Ford to pursue car building.",
             "Edison invented the entire electric utility system.",
-	    "Edison read the Hume's "History of England." Sear's "History of the World," Gibbons's "Rome," "The Penny Encyclopedia," and several works on electricity and science during the age of 9 through 12.", 
+			"Edison read the Hume's "History of England." Sear's "History of the World," Gibbons's "Rome," "The Penny Encyclopedia," and several works on electricity and science during the age of 9 through 12.", 
             "Edison invented a fluoroscope that he left to public domain and did not patent.",
             "He had a patent in 1885 on the transmission of wireless signals which were sold to Guglielmo Marconi (the radio pioneer).",
             "Edison invented an improvement to the telephone transmitter that was used for almost 100 years.",
             "Edison founded the American motion picture industry in the mid 1890s, but had little involvement in the business that sprang from his camera inventions."
+			
 
 
     };
